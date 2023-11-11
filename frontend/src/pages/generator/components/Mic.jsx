773688f30f2152aaf091fba16106c964a2b1b784
alanjohnck <alanjohnchako.live@gmail.com>
2023-11-11 19:41:39 +0530

    all set

--- frontend/src/pages/generator/components/Mic.jsx
+++ frontend/src/pages/generator/components/Mic.jsx
@@ -1,49 +1,39 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { ReactMic } from 'react-mic';
+import logo from '../../../assets/mic.png'
 
 // Create styles
 
 function App() {
-  const [record, setRecord] = useState(false);
-  const [file, setFile] = useState(null);
+    const [record, setRecord] = useState(false);
 
-  const startRecording = () => {
-    setRecord(true);
-  }
-
-  const stopRecording = () => {
-    setRecord(false);
-  }
-
-  const onData = (recordedBlob) => {
-    console.log('chunk of real-time data is: ', recordedBlob);
-  }
-
-  const onStop = (recordedBlob) => {
-    console.log('recordedBlob is: ', recordedBlob);
-    setFile(new File([recordedBlob.blob], 'recordedAudio.webm'));
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-
-    // Append the file to the form data
-    formData.append('file', file);
-
-    // Use axios to post a value to the backend
-    axios.post('http://127.0.0.1:8000/translate', formData)
-      .then(response => {
-        console.log('Response:', response.data); // Handle the successful response here
-      })
-      .catch(error => {
-        console.error('Error:', error); // Handle any errors that occurred during the request
-      });
-  }
+    const toggleRecording = () => {
+      setRecord(!record);
+    }
+  
+    const onData = (recordedBlob) => {
+      console.log('chunk of real-time data is: ', recordedBlob);
+    }
+  
+    const onStop = async (recordedBlob) => {
+      console.log('recordedBlob is: ', recordedBlob);
+      const file = new File([recordedBlob.blob], 'recordedAudio.webm');
+  
+      const formData = new FormData();
+      formData.append('file', file);
+      await axios.post('http://127.0.0.1:8000/translate', formData)
+        .then(response => {
+          onDataReady(response.data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
 
   return (
     <div>
+    <div className='flex '>
       <ReactMic
         record={record}
         className="sound-wave"
@@ -51,11 +41,17 @@ function App() {
         onData={onData}
         strokeColor="#000000"
         backgroundColor="#FF4081" />
-      <button onClick={startRecording} type="button">Start</button>
-      <button onClick={stopRecording} type="button">Stop</button>
+      <img
+        src={logo}
+        alt="mic-icon"
+        className="mic"
+        onClick={toggleRecording}
+        />
+    </div>
+      {/* <button onClick={stopRecording} type="button">Stop</button>
       <form onSubmit={handleSubmit}>
         <button type="submit">Upload</button>
-      </form>
+      </form> */}
     </div>
   );
 }
