d8fcd2484a238ee0c4a559898a8e64a22295016c
Arjunkrishna7356 <arjunkrishna7356@gmail.com>
2023-11-11 15:43:25 +0530

    voice section updated

---
+++ frontend/src/pages/generator/components/Mic.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../../../assets/mic.png";
+import { ReactMediaRecorder } from "react-media-recorder";
+
+function Mic() {
+  return (
+    <ReactMediaRecorder
+      audio
+      render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+        <div>
+          <div className="h-32 w-32 m-4">
+            <img
+              src={logo}
+              alt="mic-icon"
+              className="mic"
+              onClick={status === "recording" ? stopRecording : startRecording}
+            />
+          </div>
+          {/* {status === "recording" && (
+            <p>Recording...</p>
+          )} */}
+          {mediaBlobUrl && (
+            <audio src={mediaBlobUrl} controls autoPlay />
+          )}
+        </div>
+      )}
+    />
+  );
+}
+
+export default Mic;
