aa83a8781c17e22a71a36507fb89fed4a8b27276
AdithyanA <adithyanayithala@gmail.com>
2023-11-11 15:43:34 +0530

    mege

--- frontend/src/pages/generator/components/Mic.jsx
+++ frontend/src/pages/generator/components/Mic.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { ReactMic } from 'react-mic';
 import logo from "../../../assets/mic.png";
 function Mic() {
   return (
     <div>
       <div className="h-32 w-32 m-4">
-        <img src={logo} alt="mic-icon" className="mic"/>
+        <img src={logo} alt="mic-icon" className="mic" />
       </div>
     </div>
   );
